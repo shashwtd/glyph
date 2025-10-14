f89a5f3dc798f5dce16c0a248adfb2e15b9eb490
Shashwat Dubey <shashwat5590@gmail.com>
2025-10-14 23:37:12 +0530

    temp auth disable

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -48,8 +48,8 @@ export async function updateSession(request: NextRequest) {
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
-    url.pathname = '/login'
-    return NextResponse.redirect(url)
+    // url.pathname = '/login'
+    // return NextResponse.redirect(url)
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
