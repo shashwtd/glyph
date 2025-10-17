45478d8c778888f52d8073ec17a75ebcc7b96beb
Shashwat Dubey <shashwat5590@gmail.com>
2025-10-17 22:59:23 +0530

    the compress layout

---
+++ src/app/(tools)/compress/page.tsx
@@ -0,0 +1,9 @@
+import ImageCompressor from "@/components/ImageCompressor";
+
+export default function ImageCompressionPage() {
+    return (
+        <div className="relative z-10 w-full h-screen flex flex-col items-center justify-center gap-8">
+            <ImageCompressor />
+        </div>
+    );
+}
